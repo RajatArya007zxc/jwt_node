1b3f23aa81b1edd142b1f40093fdf7e8962ff9c6
RajatArya007zxc <rajatarya199zx@gmail.com>
2020-11-07 01:22:02 +0530

    adding the validation and response the correct error message(awesome explanation)

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -2,6 +2,42 @@
 ///// Now we rquire the userModel from the user.js for accessing the schema of database db
 const userModel=require('../models/user');
 
+/// the response we will getting "error ,user not created" if we add validator also .It will solve by the adding the function and improve the catch{}
+
+/// 
+const handleError=(err)=>{
+
+     console.log(err.message,err.code)
+     
+     let errors={email:'',password:''};
+
+
+ ///// duplicate error code
+ ///// this is used when any one of them (from email and password) is wrong while putting
+ if(err.code ===11000)
+{
+    errors.email="that email is Already exist"
+    
+    return errors;
+}
+
+
+
+
+     ////// ///// / / /
+     //                  Validation Error
+     if(err.message.includes('user validation failed')){ 
+        //  console.log(err)
+       // console.log(Object.values(err.errors))  //now we get the array
+
+       Object.values(err.errors).forEach(({properties})=>{    //destructure e=> e.properties  to {properties}
+
+        errors[properties.path] =properties.message
+       })
+     }
+     return errors;
+}
+
 
 module.exports.signup_get=(req,res)=>{    
     res.render('signup')
@@ -24,9 +60,12 @@ const user=await userModel.create({email,password})
  res.status(201).json(user)  //201 success 
     }
     catch(err){
-
-         console.log(err)
-         res.status(400).send('error ,user not created')
+  
+        //handleError(err);    // now we change this to variable for returning the value  (1) and change the (2)
+        var errors=handleError(err);
+        // console.log(err)
+        // res.status(400).send('error ,user not created')   (2) send back to json
+         res.status(400).json({errors})
     }
     
 
@@ -41,3 +80,52 @@ module.exports.login_post=async (req,res)=>{
     const {email,password}=req.body
     res.send('user login')
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+helpful resource
+
+
+
+it is the op of  Object.values(err.errors).forEach(({properties})=>{  console.log(properties)})
+
+
+user validation failed: email: Please Enter a Valid Email, password: Minimum Password length is 6 character undefined
+{
+  message: 'Please Enter a Valid Email',
+  type: 'user defined',
+  validator: <ref *1> [Function: isEmail] { default: [Circular *1] },
+  path: 'email',
+  value: 'sd'
+}
+{
+  validator: [Function (anonymous)],
+  message: 'Minimum Password length is 6 character',
+  type: 'minlength',
+  minlength: 6,
+  path: 'password',
+  value: '12'
+}
+
+
+
+
+
+*/
+
--- models/user.js
+++ models/user.js
@@ -1,16 +1,18 @@
 const mongoose=require('mongoose')
+const {isEmail}=require('validator')
 
-const userSchema=mongoose.Schema({
+const userSchema=new mongoose.Schema({
     email:{
         type:String,
-        required:true,
+        required:[true,'Please Enter email'],
         unique:true,
-        lowercase:true
+        lowercase:true,
+        validate:[isEmail,'Please Enter a Valid Email'] 
     },
     password:{
         type:String,
-        required:true,
-        minLength:6
+        required:[true, 'Please Enter Password'],
+        minlength:[6,'Minimum Password length is 6 character']
 
     }
 })
@@ -18,3 +20,6 @@ const userSchema=mongoose.Schema({
 const userModel=mongoose.model('user',userSchema);
 
 module.exports=userModel;
+
+
+//the validator library only useful for string
