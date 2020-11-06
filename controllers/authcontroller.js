60da4b0fc871ee688313febb67d9ec04702075d1
RajatArya007zxc <rajatarya199zx@gmail.com>
2020-11-07 00:20:20 +0530

    sending the data to cluster through postman using async,await and model

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,4 +1,9 @@
-module.exports.signup_get=(req,res)=>{
+
+///// Now we rquire the userModel from the user.js for accessing the schema of database db
+const userModel=require('../models/user');
+
+
+module.exports.signup_get=(req,res)=>{    
     res.render('signup')
 }
 
@@ -7,15 +12,29 @@ module.exports.login_get=(req,res)=>{
 }
 
 
-module.exports.signup_post=(req,res)=>{
+module.exports.signup_post=async (req,res)=>{   //////// now we change this to async(below also) because we use await (because of user who is promise and wait for some time to exexute)
+
 
     //now destructuring the data comming from the req
     const {email,password}=req.body
-    res.render('new signup')
+
+    try{
+const user=await userModel.create({email,password}) 
+
+ res.status(201).json(user)  //201 success 
+    }
+    catch(err){
+
+         console.log(err)
+         res.status(400).send('error ,user not created')
+    }
+    
+
+
 }
 
 
-module.exports.login_post=(req,res)=>{
+module.exports.login_post=async (req,res)=>{
     // console.log(req.body)
 
     //now destructuring the data comming from the req
